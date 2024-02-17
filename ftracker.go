9bf974338e2a69124adf90504650839d3384dffe
Daniyal Bulatov <dbulatov23@yandex.ru>
2024-02-18 00:24:15 +0300

    14 commit

--- ftracker.go
+++ ftracker.go
@@ -101,6 +101,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
+	height = height / cmInM
 	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed(action, duration)*kmhInMsec, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 }
 
